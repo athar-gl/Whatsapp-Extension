096c8e152d203a976d100d001295b485e32368cb
athar-gl <athar.riaz.gl@gmail.com>
2023-08-31 10:51:21 +0500

    add

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import ReactDOM from "react-dom";
-import "./popup.css"
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './popup.css'; // Import the CSS file
 
+const Tab = ({ userName, isActive, onClick }) => (
+    <div className={`tab ${isActive ? 'active' : ''}`} onClick={onClick}>
+        {userName}
+    </div>
+);
 
-const test = <img src="icon.png" />
+const App = () => {
+    const [usersData, setUsersData] = useState([]);
+    const [activeUser, setActiveUser] = useState(usersData[0] || {}); // Set a default value
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(test, root)
+    useEffect(() => {
+        // Fetch usersData from chrome.storage.local
+        chrome.storage.local.get(['usersData'], (result) => {
+            const storedUsersData = result.usersData || [];
+            setUsersData(storedUsersData);
+        });
+    }, []);
 
+    const handleUserClick = (user) => {
+        setActiveUser(user);
+    };
+
+    return (
+        <div className="app">
+            <div className="tabs-container">
+                {usersData.map((user, index) => (
+                    <Tab
+                        key={index}
+                        userName={user.userName}
+                        isActive={user === activeUser}
+                        onClick={() => handleUserClick(user)}
+                    />
+                ))}
+            </div>
+            <div className="content">
+                {activeUser.content}
+            </div>
+        </div>
+    );
+};
+
+const root = document.createElement('div');
+root.id = 'app';
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
